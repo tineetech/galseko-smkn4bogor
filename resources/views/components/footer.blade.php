904065ab0102db8d926ca616b3439051758f0bb6
zeru-program <justinebogor0609@gmail.com>
2025-12-10 21:35:02 +0700

    update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,6 +3,6 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2025-2026 <a href="#">Galseko</a>.</strong> All rights
+    <strong>Copyright &copy; 2025-2026 <a href="/">Galseko</a> By justine xi pplg 2 2025.</strong> All rights
     reserved.
   </footer>
